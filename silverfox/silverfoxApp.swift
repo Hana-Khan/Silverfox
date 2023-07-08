41c57ab966dd041302b0ca765e1c903283a68faa
Hina Khan <47082795+Hana-Khan@users.noreply.github.com>
2023-07-08 18:55:42 +0500

    Initial Commit

---
+++ silverfox/silverfoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  silverfoxApp.swift
+//  silverfox
+//
+//  Created by Hina Khan on 08/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct silverfoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
